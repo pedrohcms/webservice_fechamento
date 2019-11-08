06f956623b0945581b16cfb64f732b2e45595768
Pedro Henrique Correa Mota da Silva <pedrohcms2018@gmail.com>
2019-11-08 12:11:07 -0300

    Upload de arquivo adicionado
    Adicionei o upload de arquivos excel, rota de fechamento alterada para verbo POST

--- src/robo.py
+++ src/robo.py
@@ -4,24 +4,20 @@ import os
 
 def save_file(file):
     
-    file_name = file.split('\\')
-    file_name = file_name[-1]
-    
-    ext = file_name.split('.')
+    ext = file.filename.split('.')
     ext = ext[-1]
     
     if (ext not in app.config['ALLOWED_EXTENSIONS']):
         print('O arquivo não pode ser salvo, pois não tem o formato permitido')
     else:
-        if os.path.exists('C:\\file'):
-            print('Diretorio C:\\file existe')
-        else:
-            os.mkdir('C:\\file')
+        if not os.path.exists(app.config['UPLOAD_FOLDER']):
+            os.mkdir(app.config['UPLOAD_FOLDER'])
+        
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
         print('O arquivo foi salvo com sucesso')
 
 def process():
-    data = request.get_json()
 
-    save_file(data['file'])
+    save_file(request.files['file'])
     
     return jsonify([1, 2, 3])
